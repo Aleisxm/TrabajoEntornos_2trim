39402b3997f8a6e2972f5ab09850358e7078e12a
aleisxm <pablogayhomo@gmail.com>
2024-03-13 23:00:45 +0100

    Sumar

---
+++ src/Sumar.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Sumar {
+
+    public Sumar() {
+    }
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static double Sumar(double sum1, double sum2) {
+        double sum3 = sum1 + sum2;
+        return sum3;
+    }
+
+
+
+}
