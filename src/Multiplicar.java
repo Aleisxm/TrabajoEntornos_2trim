6458614ce3ebee20a71b7d0e85f5cdafc7cdcd98
aleisxm <pablogayhomo@gmail.com>
2024-03-13 23:00:24 +0100

    Multiplicar

---
+++ src/Multiplicar.java
@@ -0,0 +1,10 @@
+public class Multiplicar {
+    public Multiplicar() {
+    }
+
+    public static double Multiplicar(double sum1, double sum2) {
+
+        double sum3 = sum1 * sum2;
+        return sum3;
+    }
+}
