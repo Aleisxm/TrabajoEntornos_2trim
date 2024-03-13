68c5dc0750dcd6d6b5c62846b89255b5332bbc00
aleisxm <pablogayhomo@gmail.com>
2024-03-13 23:00:09 +0100

    Dividir

---
+++ src/Dividir.java
@@ -0,0 +1,10 @@
+public class Dividir {
+    public Dividir() {
+    }
+
+
+    public static double Dividir(double sum1, double sum2) {
+        double sum3 = sum1 / sum2;
+        return sum3;
+    }
+}
